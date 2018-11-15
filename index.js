79356269e23efa9455b3797a68d70c2e37b5c679
zhanghuaqiang <zhanghuaqiang@bykernel.com>
2018-11-15 10:22:12 +0800

    move db out of package

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const Web3 = require("web3");
-const dbfactory = require("./db/dbfactory");
 const BlockChainEventHandler = require("./blockchainEventHandler");
 const MessageHandler = require("./messageHandler");
 const BlockchainProxy = require('./blockchainProxy');
